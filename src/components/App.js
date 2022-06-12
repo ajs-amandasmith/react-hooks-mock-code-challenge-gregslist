7f12782f7a155df94ca3a3e8438bb6569c89da5a
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-12 18:33:01 -0500

    Shows Listings, Favorites Listings, Deletes a Listing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(r => r.json())
+      .then(items => setListings(items))
+  },
+  []
+  )
+
+  function deleteListing(id) {
+    const newListings = listings.filter(listing => listing.id !== id);
+    setListings(newListings);
+  }
+  
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} deleteListing={deleteListing} />
     </div>
   );
 }
 
 export default App;
+
+
+// I can search for listings by their name.
+
+
+// Advanced Deliverables
+// These deliverables are not required to pass the code challenge, but if you have the extra time, or even after the code challenge, they are a great way to stretch your skills.
+
+// You'll have to add additional elements for these features. Feel free to style them however you see fit!
+
+// Note: If you are going to attempt these advanced deliverables, please be sure to have a working commit with all the Core Deliverables first!
+
+// As a user:
+
+// I can sort the listings alphabetically by location.
+// I can create a new listing by submitting a form, and persist the changes to the backend.
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({ id, description, image="https://via.placeholder.com/300x300", location, deleteListing }) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  function handleClick(e) {
+    setIsLiked(!isLiked);
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE",
+    })
+      .then(r => r.json())
+      .then(() => deleteListing(id))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isLiked ? (
+          <button className="emoji-button favorite active" onClick={handleClick}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={handleClick}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={handleDeleteClick}>🗑</button>
       </div>
     </li>
   );
